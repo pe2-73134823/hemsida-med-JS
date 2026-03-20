dd093ee9bedbc75c84f92ef531d9a9b97d76b727
pe2-73134823 <235252853+pe2-73134823@users.noreply.github.com>
2026-03-20 10:33:38 +0100

    Store item 3 and balancing

--- script.js
+++ script.js
@@ -9,21 +9,24 @@ let manualClickMult = 0;
 manualClickMult = firstload(manualClickMult, "manualClickMult");
 let autoClickerRate = 0;
 autoClickerRate = firstload(autoClickerRate, "autoClickerRate");
+let totalMult = 0;
+totalMult = firstload(totalMult, "totalMult");
 
 // onload
 counter.textContent = "Clicks: " + clickCount
-setInterval(gameTick, 100);
+setInterval(gameTick, 50);
 setInterval(saveTick, 10000);
 
 // main game
 function gameTick() {
-    clickCount = clickCount + autoClickerRate
+    clickCount += (autoClickerRate * totalMult)
     counter.textContent = "Clicks: " + parseInt(clickCount)
 }
 function saveTick() {
     localStorage.setItem('clickCount', clickCount);
     localStorage.setItem('manualClickMult', manualClickMult);
     localStorage.setItem('autoClickerRate', autoClickerRate);
+    localStorage.setItem('totalMult', totalMult);
 }
 function clicked() {
     clickCount = clickCount + 1 * (manualClickMult + 1)
@@ -32,15 +35,21 @@ function clicked() {
 
 // store
 function storeItem1() {
-    if (clickCount > 149) {
-        clickCount = clickCount - 150
+    if (clickCount > 24) {
+        clickCount = clickCount - 25
         manualClickMult++
     }
 }
 function storeItem2() {
-    if (clickCount > 299) {
-        clickCount = clickCount - 300
-        autoClickerRate += 0.05
+    if (clickCount > 74) {
+        clickCount = clickCount - 75
+        autoClickerRate += 0.06
+    }
+}
+function storeItem3() {
+    if (clickCount > 149) {
+        clickCount = clickCount - 150
+        totalMult += 0.5
     }
 }
 
